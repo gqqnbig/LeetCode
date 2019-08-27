ceb3c2039f5f359cc421a131ec8f503972ab17e7
gqqnbig <gqqnb2005@gmail.com>
2019-08-26 22:05:39 -0700

    进一步修改

--- Responses/StrongPasswordChecker.cs
+++ Responses/StrongPasswordChecker.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace LeetCode
 {
@@ -17,61 +13,71 @@ namespace LeetCode
 		/// <returns></returns>
 		public int StrongPasswordChecker(string s)
 		{
-			int toDelete = 0;
-			if (s.Length > 20)
-				toDelete = s.Length - 20; //要删除这么多字符
-
-			int toAdd = 0;
-			int freeCharacters = 0;
-			if (s.Length < 6)
+			int toDelete = s.Length > 20 ? s.Length - 20 : 0; //要删除这么多字符
+			var sortedLength = FindRepeatingGroups(s).ToList();
+			//可以通过删除重复字符，即缩短密码，又断开连续字符
+			sortedLength.Sort((a, b) => -a.CompareTo(b));
+			int freeDeletion = toDelete;
+			while (freeDeletion > 0 && sortedLength.Count > 0)
 			{
-				toAdd = 6 - s.Length; //要补上这么多字符
-				freeCharacters = toAdd; //有这么多字符可以用来满足大小写要求。
+				//找到长度为3的倍数且至少为6的重复组，它本来要求修改2次，删除一个字后只需修改1次。
+				for (int i = 0; i < sortedLength.Count && freeDeletion > 0; i++)
+				{
+					if (sortedLength[i] / 3 * 3 == sortedLength[i])
+					{
+						sortedLength[i] -= 1;
+						freeDeletion--;
+					}
+				}
+
+				//把剩余的freeDeletion分配给所有重复组
+				for (int i = 0; i < sortedLength.Count && freeDeletion > 0; i++)
+				{
+					if (sortedLength[i] >= 3)
+					{
+						sortedLength[i] -= 1;
+						freeDeletion--;
+					}
+				}
+
+				sortedLength = sortedLength.Where(v => v > 2).ToList();
 			}
 
-			Debug.Assert(toDelete == 0 || toAdd == 0);
+
+			//repeatingGroupIndex指向尚未处理的字符组，现在需要使用modify
+			int toModify = sortedLength.Select(v => v / 3).Sum();
+			int freeModify = toModify;
+			int toAdd = 0;
 
 			if (s.Any(char.IsLower) == false)
 			{
-				if (freeCharacters > 0)
-					freeCharacters--; //其中一个新增的字符必须是小写
+				if (freeModify > 0)
+					freeModify--; //其中一个新增的字符必须是小写
 				else
 					toAdd++;
 			}
 
 			if (s.Any(char.IsUpper) == false)
 			{
-				if (freeCharacters > 0)
-					freeCharacters--; //其中一个新增的字符必须是大写
+				if (freeModify > 0)
+					freeModify--; //其中一个新增的字符必须是大写
 				else
 					toAdd++;
 			}
 
 			if (s.Any(char.IsDigit) == false)
 			{
-				if (freeCharacters > 0)
-					freeCharacters--; //其中一个新增的字符必须是数字
+				if (freeModify > 0)
+					freeModify--; //其中一个新增的字符必须是数字
 				else
 					toAdd++;
 			}
 
-			return toDelete + toAdd + ModifyToBreak(s);
-		}
+			//添加了toAdd个字符，但长度可能还是不足6，要补足。
+			if (s.Length + toAdd < 6)
+				toAdd = 6 - s.Length;
 
-
-		bool IsStrong(string password)
-		{
-			if (password.Length < 6 || password.Length > 20)
-				return false;
-
-			if ((password.Any(char.IsLower) && password.Any(char.IsUpper) && password.Any(char.IsDigit)) == false)
-				return false;
-
-			Regex repeatingCheck = new Regex(@"(.)(\1){2,}");
-			if (repeatingCheck.IsMatch(password))
-				return false;
-
-			return true;
+			return toDelete + toModify + toAdd;
 		}
 
 		/// <summary>
@@ -79,18 +85,22 @@ namespace LeetCode
 		/// </summary>
 		/// <param name="s"></param>
 		/// <returns></returns>
-		public int ModifyToBreak(string s)
+		internal int ModifyToBreak(string s)
+		{
+			return FindRepeatingGroups(s).Select(v => v / 3).Sum();
+		}
+
+		internal int RemoveToBreak(string s)
+		{
+			return FindRepeatingGroups(s).Select(v => v - 2).Sum();
+		}
+
+		private static IEnumerable<int> FindRepeatingGroups(string s)
 		{
 			Regex repeatingCheck = new Regex(@"(.)(\1){2,}");
 			MatchCollection matchCollection = repeatingCheck.Matches(s);
-			if (matchCollection.Count == 0)
-				return 0;
-			else
-			{
-				return (from Match m in matchCollection
-						select m.Value.Length / 3).Sum();
-			}
+			return from Match m in matchCollection
+				   select m.Value.Length;
 		}
-
 	}
 }
--- Tests/CStrongPasswordCheckerTests.cs
+++ Tests/CStrongPasswordCheckerTests.cs
@@ -16,9 +16,17 @@ namespace LeetCode.Tests
 
 
 		[Theory]
-		[InlineData(0,"1aA234567890")]
+		[InlineData(3, "...")]
+		[InlineData(0, "1aA234567890")]
 		[InlineData(1, "1aA234567888")]
 		[InlineData(1, "Abcdefghijklmnopqr111")]
+		[InlineData(2, "aaaaaa")]
+		[InlineData(5, "a")]
+		[InlineData(6, "")]
+		[InlineData(7, "aaaaaaaaaaaaaaaaaaaaa")]
+		[InlineData(9, "a00abb0Aa0A0aa0abA0bAb00cca0b")]
+		[InlineData(9, "aaaaaaaa11aAa1a1aaaa1aaa1aaa")]
+		[InlineData(11, "aaaaaaaa11aAa1a1aaaa1aaa1aaaCDE")]
 		public void StrongPasswordCheckerTest(int modifications, string password)
 		{
 			output.WriteLine(modifications.ToString());
@@ -26,7 +34,7 @@ namespace LeetCode.Tests
 		}
 
 		[Theory]
-		[InlineData(0,"1234")]
+		[InlineData(0, "1234")]
 		[InlineData(0, "")]
 		[InlineData(0, "a")]
 		[InlineData(0, "aa")]
@@ -41,24 +49,65 @@ namespace LeetCode.Tests
 
 		}
 
+		[Theory]
+		[InlineData(0, "1234")]
+		[InlineData(0, "")]
+		[InlineData(0, "a")]
+		[InlineData(0, "aa")]
+		[InlineData(1, "aaa")]
+		[InlineData(2, "aaaa")]
+		[InlineData(3, "aaaaa")]
+		[InlineData(4, "aaaaaa")]
+		[InlineData(2, "aaa1bbb")]
+		public void RemoveToBreakTest(int modifications, string password)
+		{
+			Assert.Equal(modifications, new CStrongPasswordChecker().RemoveToBreak(password));
+
+		}
+
 		[Fact]
 		public void StrongPasswordCheckerGenerationTest()
 		{
-			char[] symbols = {'0', 'a', 'A', 'b', 'c'};
-			Random rand=new Random();
+			char[] symbols = { '0', 'a', 'A', 'b', 'c' };
+			Random rand = new Random();
 			var checker = new CStrongPasswordChecker();
 			for (int iteration = 0; iteration < 20; iteration++)
 			{
-				int length= rand.Next(1, 30);
-				StringBuilder sb=new StringBuilder(length);
+				int length = rand.Next(1, 30);
+				StringBuilder sb = new StringBuilder(length);
 				for (int i = 0; i < length; i++)
 				{
 					sb.Append(symbols[rand.Next(symbols.Length)]);
 				}
 
 				string password = sb.ToString();
+				output.WriteLine("Testing {0}", password);
 				int modifications = checker.StrongPasswordChecker(password);
-				output.WriteLine("{0} needs to modify {1} times.", password,modifications);
+				output.WriteLine("{0} needs to modify {1} times.", password, modifications);
+			}
+		}
+
+		[Fact]
+		public void StrongPasswordCheckerSameCharacterTest()
+		{
+			var checker = new CStrongPasswordChecker();
+			Random rand = new Random();
+			for (int iteration = 0; iteration < 30; iteration++)
+			{
+				StringBuilder sb = new StringBuilder(new string('a', iteration));
+
+				int insert = rand.Next(iteration / 3 * 2);
+				for (int i = insert; i > 0; i--)
+				{
+					sb.Insert(rand.Next(sb.Length), '1');
+				}
+
+				if (rand.Next(100) > 80)
+					sb.Insert(rand.Next(sb.Length), 'A');
+
+				string password = sb.ToString();
+				int modifications = checker.StrongPasswordChecker(password);
+				output.WriteLine("{0}({2}) needs to modify {1} times.", password, modifications, password.Length);
 			}
 		}
 
