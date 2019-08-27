d0f94a8c0ef954adbd2eff97375288fd13d90c01
gqqnbig <gqqnb2005@gmail.com>
2019-08-26 18:36:39 -0700

    420. Strong Password Checker

---
+++ Responses/StrongPasswordChecker.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+	public class CStrongPasswordChecker
+	{
+		/// <summary>
+		/// Return the minimum changes requires to make a password strong.
+		/// </summary>
+		/// <param name="s"></param>
+		/// <returns></returns>
+		public int StrongPasswordChecker(string s)
+		{
+			int toDelete = 0;
+			if (s.Length > 20)
+				toDelete = s.Length - 20; //要删除这么多字符
+
+			int toAdd = 0;
+			int freeCharacters = 0;
+			if (s.Length < 6)
+			{
+				toAdd = 6 - s.Length; //要补上这么多字符
+				freeCharacters = toAdd; //有这么多字符可以用来满足大小写要求。
+			}
+
+			Debug.Assert(toDelete == 0 || toAdd == 0);
+
+			if (s.Any(char.IsLower) == false)
+			{
+				if (freeCharacters > 0)
+					freeCharacters--; //其中一个新增的字符必须是小写
+				else
+					toAdd++;
+			}
+
+			if (s.Any(char.IsUpper) == false)
+			{
+				if (freeCharacters > 0)
+					freeCharacters--; //其中一个新增的字符必须是大写
+				else
+					toAdd++;
+			}
+
+			if (s.Any(char.IsDigit) == false)
+			{
+				if (freeCharacters > 0)
+					freeCharacters--; //其中一个新增的字符必须是数字
+				else
+					toAdd++;
+			}
+
+			return toDelete + toAdd + ModifyToBreak(s);
+		}
+
+
+		bool IsStrong(string password)
+		{
+			if (password.Length < 6 || password.Length > 20)
+				return false;
+
+			if ((password.Any(char.IsLower) && password.Any(char.IsUpper) && password.Any(char.IsDigit)) == false)
+				return false;
+
+			Regex repeatingCheck = new Regex(@"(.)(\1){2,}");
+			if (repeatingCheck.IsMatch(password))
+				return false;
+
+			return true;
+		}
+
+		/// <summary>
+		/// 返回最少的修改次数，以便断开一个连续的相同字符串
+		/// </summary>
+		/// <param name="s"></param>
+		/// <returns></returns>
+		public int ModifyToBreak(string s)
+		{
+			Regex repeatingCheck = new Regex(@"(.)(\1){2,}");
+			MatchCollection matchCollection = repeatingCheck.Matches(s);
+			if (matchCollection.Count == 0)
+				return 0;
+			else
+			{
+				return (from Match m in matchCollection
+						select m.Value.Length / 3).Sum();
+			}
+		}
+
+	}
+}
---
+++ Tests/CStrongPasswordCheckerTests.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Text;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace LeetCode.Tests
+{
+	public class CStrongPasswordCheckerTests
+	{
+		private readonly ITestOutputHelper output;
+
+		public CStrongPasswordCheckerTests(ITestOutputHelper output)
+		{
+			this.output = output;
+		}
+
+
+		[Theory]
+		[InlineData(0,"1aA234567890")]
+		[InlineData(1, "1aA234567888")]
+		[InlineData(1, "Abcdefghijklmnopqr111")]
+		public void StrongPasswordCheckerTest(int modifications, string password)
+		{
+			output.WriteLine(modifications.ToString());
+			Assert.Equal(modifications, new CStrongPasswordChecker().StrongPasswordChecker(password));
+		}
+
+		[Theory]
+		[InlineData(0,"1234")]
+		[InlineData(0, "")]
+		[InlineData(0, "a")]
+		[InlineData(0, "aa")]
+		[InlineData(1, "aaa")]
+		[InlineData(1, "aaaa")]
+		[InlineData(1, "aaaaa")]
+		[InlineData(2, "aaaaaa")]
+		[InlineData(2, "aaa1bbb")]
+		public void ModifyToBreakTest(int modifications, string password)
+		{
+			Assert.Equal(modifications, new CStrongPasswordChecker().ModifyToBreak(password));
+
+		}
+
+		[Fact]
+		public void StrongPasswordCheckerGenerationTest()
+		{
+			char[] symbols = {'0', 'a', 'A', 'b', 'c'};
+			Random rand=new Random();
+			var checker = new CStrongPasswordChecker();
+			for (int iteration = 0; iteration < 20; iteration++)
+			{
+				int length= rand.Next(1, 30);
+				StringBuilder sb=new StringBuilder(length);
+				for (int i = 0; i < length; i++)
+				{
+					sb.Append(symbols[rand.Next(symbols.Length)]);
+				}
+
+				string password = sb.ToString();
+				int modifications = checker.StrongPasswordChecker(password);
+				output.WriteLine("{0} needs to modify {1} times.", password,modifications);
+			}
+		}
+
+
+	}
+}
