0515e475f69d555457c97f45362a7bf001515a0b
gqqnbig <gqqnb2005@gmail.com>
2019-09-01 15:54:58 -0700

    166. Fraction to Recurring Decimal

---
+++ Tests/166-Fraction to Recurring Decimal-Tests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Tests
+{
+	[TestClass]
+	public class FractionToRecurringDecimalTests
+	{
+		[DataTestMethod]
+		[DataRow("", int.MaxValue - 1, int.MaxValue)]
+		[DataRow("1", int.MaxValue, int.MaxValue)]
+		[DataRow("2.(3)", 7, 3)]
+		public void FractionToDecimalTest(string expected, int numerator, int denominator)
+		{
+			Assert.AreEqual(expected, new FractionToRecurringDecimal().FractionToDecimal(numerator, denominator));
+		}
+	}
+}
---
+++ Tests/166-Fraction to Recurring Decimal.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Tests
+{
+	class FractionToRecurringDecimal
+	{
+		public string FractionToDecimal(int numerator, int denominator)
+		{
+			var d = GetCommonDivisor(numerator, denominator);
+			numerator /= d;
+			denominator /= d;
+
+			BitArray arr = new BitArray(denominator);
+			StringBuilder sb = new StringBuilder();
+
+			var quotient = numerator / denominator;
+			sb.Append(quotient);
+			numerator = numerator % denominator;
+
+			//进入小数部分
+			if (numerator != 0)
+				sb.Append(".");
+
+			while (numerator != 0)
+			{
+				if (arr[numerator])
+				{
+					sb.Append(")");
+					return sb.ToString();
+				}
+				else
+					arr[numerator] = true;
+
+				numerator *= 10;
+				sb.Append(numerator / denominator);
+				numerator = numerator % denominator;
+			}
+
+			return sb.ToString();
+
+		}
+
+		int GetCommonDivisor(int num1, int num2)
+		{
+			//辗转相除法
+			int remainder = num1;
+			while (num1 % num2 > 0)
+			{
+				remainder = num1 % num2;
+				num1 = num2;
+				num2 = remainder;
+			}
+			return remainder;
+		}
+	}
+}
