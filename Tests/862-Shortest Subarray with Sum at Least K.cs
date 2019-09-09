d314744591ff703195e3a402f8cdf437d8d325af
gqqnbig <gqqnb2005@gmail.com>
2019-09-09 14:41:33 -0700

    完成，但会超时

--- Tests/862-Shortest Subarray with Sum at Least K.cs
+++ Tests/862-Shortest Subarray with Sum at Least K.cs
@@ -8,9 +8,36 @@ namespace LeetCode.Tests
 {
 	class ShortestSubarrayWithSumAtLeastK
 	{
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="A"></param>
+		/// <param name="K">sum</param>
+		/// <returns></returns>
 		public int ShortestSubarray(int[] A, int K)
 		{
+			int shortestLength = int.MaxValue;
+			for (int i = 0; i < A.Length; i++)
+			{
+				int sum = 0;
+				for (int j = i; j < A.Length; j++)
+				{
+					sum += A[j];
 
+					if (sum >= K)
+					{
+						int l = j - i + 1;
+						if (l < shortestLength)
+							shortestLength = l;
+						break;
+					}
+				}
+			}
+
+			if (shortestLength == int.MaxValue)
+				return -1;
+			else
+				return shortestLength;
 		}
 	}
 }
