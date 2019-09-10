773e6b1230f33d8e2ba6e5b96c35212522e4ddda
gqqnbig <gqqnb2005@gmail.com>
2019-09-10 14:00:42 -0700

    另一种方法完成

--- Tests/862-Shortest Subarray with Sum at Least K.cs
+++ Tests/862-Shortest Subarray with Sum at Least K.cs
@@ -10,6 +10,7 @@ namespace LeetCode.Tests
 {
 	class ShortestSubarrayWithSumAtLeastK
 	{
+
 		/// <summary>
 		/// 
 		/// </summary>
@@ -18,8 +19,133 @@ namespace LeetCode.Tests
 		/// <returns></returns>
 		public int ShortestSubarray(int[] A, int K)
 		{
+			//stickyData[i]表示从i开始的子数组，最大的和为stickyData[i].Sum，由stickyData[i].Length个元素构成。
+			SumLength[] stickyData = new SumLength[A.Length];
+			stickyData[A.Length - 1] = new SumLength { Sum = A[A.Length - 1], Length = 1 };
+
+			//int[] compactA = new int[A.Length];
+			//Array.Copy(A, compactA, A.Length);
+			//CompactArray(compactA);
+
+			//SumLength[] nonStickyData = new SumLength[A.Length];
+			//nonStickyData[A.Length - 1] = new SumLength { Sum = int.MinValue };
+
+			//SumLength[] overallData = new SumLength[A.Length];
+			//overallData[A.Length-1] = new SumLength { Sum = A[A.Length - 1], Length = 1 };
+
+
+			//for (int i = A.Length - 2; i >= 0; i--)
+			//{
+			//	if (A[i] > A[i] + stickyData[i + 1].Sum)
+			//		stickyData[i] = new SumLength { Sum = A[i], Length = 1 };
+			//	else
+			//	{
+			//		var data = new SumLength { Sum = A[i] + stickyData[i + 1].Sum, Length = stickyData[i + 1].Length + 1 };
+			//		int t = i + data.Length - 1;
+			//		while (data.Sum - A[t] > K)
+			//		{
+			//			//舍弃尾部
+			//			data.Sum -= A[t];
+			//			data.Length--;
+			//			t--;
+			//		}
+			//		stickyData[i] = data;
+			//	}
+
+			//	if(stickyData[i+1]
+
+			//	nonStickyData[i]
+			//}
+
+
+
+
+
+
+
+			for (int i = A.Length - 2; i >= 0; i--)
+			{
+				if (A[i] > A[i] + stickyData[i + 1].Sum)
+					stickyData[i] = new SumLength { Sum = A[i], Length = 1 };
+				else
+				{
+					var data = new SumLength { Sum = A[i] + stickyData[i + 1].Sum, Length = stickyData[i + 1].Length + 1 };
+					int t = i + data.Length - 1;
+					var s = data.Sum;
+					while (t > i) //当compactA[t]==0时，t可能减到小于i。
+					{
+						s -= A[t];
+						t--;
+						if (s >= K)
+						{
+							data.Sum = s;
+							data.Length = t - i + 1;
+						}
+					}
+					stickyData[i] = data;
+				}
+			}
+
+			int length = stickyData.Where(d => d.Sum >= K).Select(d => d.Length).DefaultIfEmpty(-1).Min();
+			return length;
+		}
+
+		/// <summary>
+		/// 除了最后一个元素可能是负数外，其他元素都非负。
+		/// </summary>
+		/// <param name="A"></param>
+		public static void CompactArray(int[] A)
+		{
+			for (int i = 0; i < A.Length - 1; i++)
+			{
+				if (A[i] < 0)
+				{
+					A[i + 1] += A[i];
+					A[i] = 0;
+
+					//for (int j = i + 1; j >= 0; j--)
+					//{
+					//	if (A[j] < 0)
+					//	{
+					//		A[i] += A[j];
+					//		A[j] = 0;
+					//	}
+					//	else
+					//	{
+					//		i = j + 1;
+					//		break;
+					//	}
+					//}
+				}
+			}
+
+
+		}
+
+
+		[DebuggerDisplay("Sum={Sum}, Length={Length}")]
+		struct SumLength
+		{
+			public int Sum { get; set; }
+			public int Length { get; set; }
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="A"></param>
+		/// <param name="K">sum</param>
+		/// <returns></returns>
+		public int ShortestSubarray2(int[] A, int K)
+		{
+			int[] maximumPossible = new int[A.Length];
+			maximumPossible[A.Length - 1] = A[A.Length - 1];
+			for (int i = A.Length - 2; i >= 0; i--)
+				maximumPossible[i] = Math.Max(A[i], A[i] + maximumPossible[i + 1]);
+
+
 			var r = ShortestSubarray(A, 0, K, new Dictionary<int, int>[A.Length], new Dictionary<int, int>[A.Length],
-									 Enumerable.Repeat(int.MaxValue, A.Length).ToArray(), false);
+									 maximumPossible, false);
 			if (r == int.MaxValue)
 				return -1;
 			else
@@ -33,13 +159,13 @@ namespace LeetCode.Tests
 		/// <param name="startIndex"></param>
 		/// <param name="K"></param>
 		/// <param name="stickyMap"></param>
-		/// <param name="minimalImpossible">记录最小返回-1的K。如果在相同位置传入了更大的K，则更不可能满足条件了。</param>
+		/// <param name="maximumPossible">在子数组包含index的情况下，记录最小返回-1的K。如果在相同位置传入了更大的K，则更不可能满足条件了。</param>
 		/// <returns></returns>
-		int ShortestSubarray(int[] A, int startIndex, int K, Dictionary<int, int>[] stickyMap, Dictionary<int, int>[] freeMap, int[] minimalImpossible, bool stickToStart)
+		int ShortestSubarray(int[] A, int startIndex, int K, Dictionary<int, int>[] stickyMap, Dictionary<int, int>[] freeMap, int[] maximumPossible, bool stickToStart)
 		{
 			if (startIndex >= A.Length)
 				return int.MaxValue;
-			if (K >= minimalImpossible[startIndex])
+			if (stickToStart && K > maximumPossible[startIndex])
 				return int.MaxValue;
 			if (A[startIndex] >= K)
 				return 1;
@@ -50,37 +176,84 @@ namespace LeetCode.Tests
 			if (map[startIndex]?.TryGetValue(K, out shortestLength) ?? false)
 				return shortestLength;
 
-			shortestLength = ShortestSubarray(A, startIndex + 1, K - A[startIndex], stickyMap, freeMap, minimalImpossible, true);
+			shortestLength = ShortestSubarray(A, startIndex + 1, K - A[startIndex], stickyMap, freeMap, maximumPossible, true);
 			if (shortestLength < int.MaxValue)
 				shortestLength++; //加上A[startIndex]这个元素
 
 			if (stickToStart == false)
 			{
-				for (int i = startIndex + 1; i < A.Length; i++)
+				var l = ShortestSubarray(A, startIndex + 1, K, stickyMap, freeMap, maximumPossible, false);
+				if (l < shortestLength)
+					shortestLength = l;
+			}
+
+
+			//if (shortestLength < int.MaxValue)
+			//{
+
+			//}
+			//else
+			//{
+			//	Debug.Assert(maximumPossible[startIndex] > K);
+			//	maximumPossible[startIndex] = K;
+			//}
+			if (map[startIndex] == null)
+				map[startIndex] = new Dictionary<int, int>();
+			map[startIndex].Add(K, shortestLength);
+			return shortestLength;
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="A"></param>
+		/// <param name="index"></param>
+		/// <param name="K"></param>
+		/// <param name="stickyMap"></param>
+		/// <param name="minimalImpossible">记录最小返回-1的K。如果在相同位置传入了更大的K，则更不可能满足条件了。</param>
+		/// <returns></returns>
+		int ShortestSubarraySticky(int[] A, int startIndex, int K, Dictionary<int, int>[] stickyMap, int[] minimalImpossible)
+		{
+			int index = startIndex;
+			int l;
+			while (true)
+			{
+				if (index >= A.Length)
+					return int.MaxValue;
+				if (K >= minimalImpossible[index])
+					return int.MaxValue;
+				if (A[index] >= K)
 				{
-					var l = ShortestSubarray(A, i, K, stickyMap, freeMap, minimalImpossible, false);
-					if (l < shortestLength)
-					{
-						shortestLength = l;
-						if (shortestLength == 1)
-							break;
-					}
+					l = index - startIndex;
+					break;
+				}
+
+
+				int shortestLength = 0;
+				if (stickyMap[index]?.TryGetValue(K, out shortestLength) ?? false)
+				{
+					l = index - startIndex + shortestLength;
+					break;
+				}
+
+				shortestLength = ShortestSubarraySticky(A, index + 1, K - A[index], stickyMap, minimalImpossible);
+				if (shortestLength < int.MaxValue)
+					shortestLength++; //加上A[startIndex]这个元素
+
+
+				if (shortestLength < int.MaxValue)
+				{
+					if (stickyMap[index] == null)
+						stickyMap[index] = new Dictionary<int, int>();
+					stickyMap[index].Add(K, shortestLength);
+				}
+				else
+				{
+					Debug.Assert(minimalImpossible[index] > K);
+					minimalImpossible[index] = K;
 				}
 			}
-
-
-			if (shortestLength < int.MaxValue)
-			{
-				if (map[startIndex] == null)
-					map[startIndex] = new Dictionary<int, int>();
-				map[startIndex].Add(K, shortestLength);
-			}
-			else
-			{
-				Debug.Assert(minimalImpossible[startIndex] > K);
-				minimalImpossible[startIndex] = K;
-			}
-			return shortestLength;
+			return l;
 		}
 
 
