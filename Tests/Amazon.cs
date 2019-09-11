fd948ec0fa1bff618363785dce9686260f6eba30
gqqnbig <gqqnb2005@gmail.com>
2019-09-10 21:54:20 -0700

    通过

--- Tests/Amazon.cs
+++ Tests/Amazon.cs
@@ -20,25 +20,129 @@ namespace LeetCode.Tests
 
 		public int minimumTime(int numOfParts, int[] parts)
 		{
-			SortedSet<int> list = new SortedSet<int>();
+			MinHeap heap=new MinHeap(parts.Length);
+
 			for (int i = 0; i < parts.Length; i++)
 			{
-				list.Add(parts[i]);
+				heap.Add(parts[i]);
 			}
 
 			var sum = 0;
-			while (list.Count > 1)
+			while (heap.Count > 1)
 			{
-				var t1 = list.Min;
-				list.Remove(t1);
-				var t2 = list.Min;
-				list.Remove(t2);
+				var t1 = heap.Pop();
+				var t2 = heap.Pop();
 
 				sum += t1 + t2;
-				list.Add(t1 + t2);
+				heap.Add(t1 + t2);
 			}
 
 			return sum;
+		}
+
+		public class MinHeap
+		{
+			private readonly int[] _elements;
+			private int _size;
+
+			public MinHeap(int size)
+			{
+				_elements = new int[size];
+			}
+
+			public int Count
+			{
+				get { return _size; }
+			}
+
+			private int GetLeftChildIndex(int elementIndex) => 2 * elementIndex + 1;
+			private int GetRightChildIndex(int elementIndex) => 2 * elementIndex + 2;
+			private int GetParentIndex(int elementIndex) => (elementIndex - 1) / 2;
+
+			private bool HasLeftChild(int elementIndex) => GetLeftChildIndex(elementIndex) < _size;
+			private bool HasRightChild(int elementIndex) => GetRightChildIndex(elementIndex) < _size;
+			private bool IsRoot(int elementIndex) => elementIndex == 0;
+
+			private int GetLeftChild(int elementIndex) => _elements[GetLeftChildIndex(elementIndex)];
+			private int GetRightChild(int elementIndex) => _elements[GetRightChildIndex(elementIndex)];
+			private int GetParent(int elementIndex) => _elements[GetParentIndex(elementIndex)];
+
+			private void Swap(int firstIndex, int secondIndex)
+			{
+				var temp = _elements[firstIndex];
+				_elements[firstIndex] = _elements[secondIndex];
+				_elements[secondIndex] = temp;
+			}
+
+			public bool IsEmpty()
+			{
+				return _size == 0;
+			}
+
+			public int Peek()
+			{
+				if (_size == 0)
+					throw new IndexOutOfRangeException();
+
+				return _elements[0];
+			}
+
+			public int Pop()
+			{
+				if (_size == 0)
+					throw new IndexOutOfRangeException();
+
+				var result = _elements[0];
+				_elements[0] = _elements[_size - 1];
+				_size = _size - 1;
+
+				ReCalculateDown();
+
+				return result;
+			}
+
+			public void Add(int element)
+			{
+				if (_size == _elements.Length)
+					throw new IndexOutOfRangeException();
+
+				_elements[_size] = element;
+				_size = _size + 1;
+
+				ReCalculateUp();
+			}
+
+			private void ReCalculateDown()
+			{
+				int index = 0;
+				while (HasLeftChild(index))
+				{
+					var smallerIndex = GetLeftChildIndex(index);
+					if (HasRightChild(index) && GetRightChild(index) < GetLeftChild(index))
+					{
+						smallerIndex = GetRightChildIndex(index);
+					}
+
+					if (_elements[smallerIndex] >= _elements[index])
+					{
+						break;
+					}
+
+					Swap(smallerIndex, index);
+					index = smallerIndex;
+				}
+			}
+
+			private void ReCalculateUp()
+			{
+				var index = _size - 1;
+				while (!IsRoot(index) && _elements[index] < GetParent(index))
+				{
+					var parentIndex = GetParentIndex(index);
+					Swap(parentIndex, index);
+					index = parentIndex;
+				}
+			}
 		}
 
 
