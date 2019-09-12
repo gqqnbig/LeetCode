9c73fb19aecbcb7eb99fc15bae5e147970112913
gqqnbig <gqqnb2005@gmail.com>
2019-09-12 01:03:04 -0700

    Next jump 算法题

--- Tests/239-Sliding Window Maximum.cs
+++ Tests/239-Sliding Window Maximum.cs
@@ -26,40 +26,26 @@ namespace LeetCode.Tests
 			if (nums.Length == 0)
 				return new int[0];
 			int[] result = new int[nums.Length - k + 1];
-			//用链表实现单调队列，因为需要从两端移除元素。
-			//设尾端为peek。
-			//设该队列为单调递减队列，First为最大值，Last为最大值。
-			var monotoneQueue = new LinkedList<Data>();
+
+			//通过比较器设置队列为单调递减
+			var comparer = Comparer<Data>.Create((a, b) => b.Value.CompareTo(a.Value));
+			var monotoneQueue = new MonotoneQueue<Data>(comparer);
 
 			//初始化单调队列，同时也是处理初始窗口
 			for (int i = 0; i < k - 1; i++)
-				Enqueue(monotoneQueue, new Data(i, nums[i]));
+				monotoneQueue.Enqueue(new Data(i, nums[i]));
 
 			for (int i = 0; i < result.Length; i++)
 			{
-				Enqueue(monotoneQueue, new Data(i + k - 1, nums[i + k - 1]));
-				result[i] = monotoneQueue.First.Value.Value;
+				monotoneQueue.Enqueue(new Data(i + k - 1, nums[i + k - 1]));
+				result[i] = monotoneQueue.Head.Value;
 
-				Debug.Assert(monotoneQueue.First.Value.Index >= i);
-				if (monotoneQueue.First.Value.Index == i)
-					monotoneQueue.RemoveFirst();
+				Debug.Assert(monotoneQueue.Head.Index >= i);
+				if (monotoneQueue.Head.Index == i)
+					monotoneQueue.Dequeue();
 			}
 
 			return result;
-		}
-
-		/// <summary>
-		/// Add an item to monotonic decreasing queue. If the item is larger than <see cref="LinkedList{T}.Last"/>,
-		/// <see cref="LinkedList{T}.Last"/> will be removed.
-		/// </summary>
-		/// <param name="monotonicDecreasingQueue"></param>
-		/// <param name="item"></param>
-		void Enqueue(LinkedList<Data> monotonicDecreasingQueue, Data item)
-		{
-			while (monotonicDecreasingQueue.Last?.Value.Value < item.Value)
-				monotonicDecreasingQueue.RemoveLast();
-
-			monotonicDecreasingQueue.AddLast(item);
 		}
 
 		[DebuggerDisplay("Index={Index}, Value={Value}")]
@@ -77,35 +63,24 @@ namespace LeetCode.Tests
 
 
 
-	public class MonotoneQueue : IEnumerable<int>
+	public class MonotoneQueue<T> : IEnumerable<T>
 	{
-		public int FixedSize { get; }
-		LinkedList<int> list = new LinkedList<int>();
-		/// <summary>
-		/// 若为true，则 <see cref="Peek"/> 是全队列最大的。
-		/// </summary>
-		public bool PeekMax { get; }
+		private readonly IComparer<T> comparer;
+		private readonly LinkedList<T> list = new LinkedList<T>();
 
 		/// <summary>
 		/// 
 		/// </summary>
-		/// <param name="peekMax">队列只允许在队尾进行插入操作。若rearMax为true，则后端元素是全队列最大的。</param>
-		public MonotoneQueue(bool peekMax) : this(peekMax, int.MaxValue)
+		/// <param name="comparer">用comparer的方法来决定单调递增还是单调递减</param>
+		public MonotoneQueue(IComparer<T> comparer)
 		{
+			this.comparer = comparer;
 		}
 
-		public MonotoneQueue(bool peekMax, int fixedSize)
+		public void Enqueue(T item)
 		{
-			FixedSize = fixedSize;
-			this.PeekMax = peekMax;
-		}
-
-		public void Enqueue(int item)
-		{
-			while (list.Count > 0 && (PeekMax && list.Last.Value >= item || PeekMax == false && list.Last.Value <= item))
+			while (list.Count > 0 && comparer.Compare(list.Last.Value, item) > 0)
 				list.RemoveLast();
-			if (list.Count >= FixedSize)
-				list.RemoveFirst();
 
 			list.AddLast(item);
 		}
@@ -114,13 +89,12 @@ namespace LeetCode.Tests
 		{
 			list.RemoveFirst();
 		}
-		/// <summary>
-		/// 若 <see cref="PeekMax"/> 为true，则Peek是全队列最大的。
-		/// </summary>
-		/// <returns></returns>
-		public int Peek => list.Last.Value;
 
-		public IEnumerator<int> GetEnumerator()
+
+		public T Head => list.First.Value;
+
+
+		public IEnumerator<T> GetEnumerator()
 		{
 			return list.GetEnumerator();
 		}
---
+++ Tests/NextJump1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Tests
+{
+	[TestClass]
+	public class NextJump1
+	{
+		/*
+		 * Complete the 'userDataWrong' function below.
+		 *
+		 * The function is expected to return a STRING_ARRAY.
+		 * The function accepts 2D_STRING_ARRAY userInfo as parameter.
+		 */
+
+		public static List<string> userDataWrong(List<List<string>> userInfo)
+		{
+			int totalCount = userInfo.Count;
+
+			var riskUserIds = (from u in userInfo
+							   where u[1] == "-1" //has risk
+							   select u[0]).ToList();
+
+			int riskyUserCount = riskUserIds.Count;
+
+
+			List<string> result = new List<string>(1 + 1 + riskyUserCount);
+			result.Add(totalCount.ToString());
+			result.Add(riskyUserCount.ToString());
+			result.AddRange(riskUserIds);
+
+			return result;
+		}
+
+		/// <summary>
+		/// Complete the 'maxMoves' function below.
+		///
+		/// The function is expected to return an INTEGER.
+		/// The function accepts following parameters:
+		///  1. STRING s
+		///  2. STRING t
+		/// </summary>
+		/// <param name="s"></param>
+		/// <param name="t">a short string. We need to remove t from s.</param>
+		/// <returns></returns>
+		public static int maxMoves(string s, string t)
+		{
+			return maxMoves(s, t, 0, new Dictionary<string, int>());
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="s"></param>
+		/// <param name="t"></param>
+		/// <param name="usedSteps"></param>
+		/// <param name="maxLeftSteps">Key is s, value is the maximum number of steps used to achieve the string.</param>
+		/// <returns></returns>
+		public static int maxMoves(string s, string t, int usedSteps, Dictionary<string, int> maxLeftSteps)
+		{
+			if (maxLeftSteps.TryGetValue(s, out var n))
+				return n;
+
+			var index1 = s.IndexOf(t);
+			if (index1 == -1)
+			{
+				maxLeftSteps[s] = 0;
+				return 0;
+			}
+
+			//remove the first occurrence
+			var s1 = s.Substring(0, index1) + s.Substring(index1 + t.Length);
+			var steps1 = maxMoves(s1, t, usedSteps + 1, maxLeftSteps) + 1;
+
+
+			var index2 = s.LastIndexOf(t);
+			if (index1 == index2) //may not need this if we have cache.
+				return steps1;
+
+			var s2 = s.Substring(0, index2) + s.Substring(index2 + t.Length);
+			if (s1 == s2)  //may not need this if we have cache.
+				return steps1;
+			var steps2 = maxMoves(s2, t, usedSteps + 1, maxLeftSteps) + 1;
+			var max = steps1 > steps2 ? steps1 : steps2;
+
+			maxLeftSteps[s] = max;
+			return max;
+		}
+
+		[DataTestMethod]
+		[DataRow(3, "bcbbc", "b")]
+		[DataRow(2, "aabb", "ab")]
+		[DataRow(1, "aabcbdc", "abc")]
+		[DataRow(0, "abab", "aa")]
+		public void maxMovesTest(int expected, string s, string t)
+		{
+			Assert.AreEqual(expected, maxMoves(s, t));
+		}
+
+	}
+}
