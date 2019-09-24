1bd57e00d50ae78dce55e7cf038a1e2c013083b6
gqqnbig <gqqnb2005@gmail.com>
2019-09-23 21:59:28 -0700

    880. Decoded String at Index 通过

--- Tests/880-Decoded String at Index-Tests.cs
+++ Tests/880-Decoded String at Index-Tests.cs
@@ -10,19 +10,19 @@ namespace LeetCode.Tests
 	[TestClass]
 	public class DecodedStringAtIndexTests
 	{
-
-		[TestMethod]
-		public void TotalLengthTest()
-		{
-			DecodedStringAtIndex.DecodeAtIndex("leet2code3");
-		}
-
 		[DataTestMethod]
-		[DataRow("o","leet2code3",10)]
-		[DataRow("t","leet2code3",20)]
-		public void Test(string expected, string S, int K)
+		[DataRow('q', "l3mtm5weq7ki78c7hck4",165511)]
+		[DataRow('b', "a2b3c4d5e6f7g8h9", 9)]
+		[DataRow('a', "abc", 1)]
+		[DataRow('a', "a2345678999999999999999", 5000)]
+		[DataRow('h', "ha22", 5)]
+		[DataRow('e', "leet2code3", 3)]
+		[DataRow('l', "leet2code3", 5)]
+		[DataRow('o', "leet2code3", 10)]
+		[DataRow('t', "leet2code3", 20)]
+		public void Test(char expected, string S, int K)
 		{
-			Assert.AreEqual(expected,new DecodedStringAtIndex().DecodeAtIndex(S,K));
+			Assert.AreEqual(expected, new DecodedStringAtIndex().DecodeAtIndexCore(S, K));
 		}
 	}
 }
--- Tests/880-Decoded String at Index.cs
+++ Tests/880-Decoded String at Index.cs
@@ -10,35 +10,48 @@ namespace LeetCode.Tests
 {
 	class DecodedStringAtIndex
 	{
+
+		public string DecodeAtIndex(string S, int K)
+		{
+			return DecodeAtIndexCore(S, K).ToString();
+		}
+
 		/// <summary>
 		/// 
 		/// </summary>
 		/// <param name="S"></param>
 		/// <param name="K">starts from 1</param>
 		/// <returns></returns>
-		public string DecodeAtIndex(string S, int K)
+		public char DecodeAtIndexCore(string S, int K)
 		{
 			var repeatings = FindRepeatings(S, K);
+			Debug.Assert(repeatings[repeatings.Count - 1].Length >= K && (repeatings.Count == 1 || repeatings[repeatings.Count - 2].Length < K),
+						 "最后一个repeatings的Length必须>=K，倒数第二个repeatings的Length必须<K。第二个分句是为了及时终止。");
 			return DecodeAtIndex(S, K, repeatings);
 		}
 
-		private string DecodeAtIndex(string S, int K, List<Repeating> repeatings)
+		private char DecodeAtIndex(string S, int K, List<Repeating> repeatings)
 		{
-			Debug.Assert(K > 0,"K从1开始。");
+			Debug.Assert(K > 0, "K从1开始。");
 
 			if (K > repeatings.Last().BaseLength)
-				return DecodeAtIndex(S, (K - 1) % repeatings.Last().BaseLength, repeatings);
+				return DecodeAtIndex(S, (K - 1) % repeatings.Last().BaseLength + 1, repeatings);
 
 			for (int i = repeatings.Count - 2; i >= 0; i--)
 			{
-				if (repeatings[i].Length < K && K <= repeatings[i + 1].BaseLength)
+				Debug.Assert(repeatings[i].Length < repeatings[i + 1].BaseLength);
+				if (repeatings[i].Length < K)
 				{
-					return S[repeatings[i].EndIndex + 1 + K - repeatings[i].Length].ToString();
+					if (K <= repeatings[i + 1].BaseLength)
+						return S[repeatings[i].EndIndex + 1 + K - repeatings[i].Length];
+					else
+						return DecodeAtIndex(S, (K - 1) % repeatings[i + 1].BaseLength + 1, repeatings);
 				}
 			}
 
-			Debug.Assert(K < repeatings[0].Length);
-			return S[K % repeatings[0].BaseLength].ToString();
+			Debug.Assert(K <= repeatings[0].Length);
+			//K是1基的，改为0基
+			return S[(K - 1) % repeatings[0].BaseLength];
 		}
 
 		private static List<Repeating> FindRepeatings(string input, int K)
@@ -54,21 +67,33 @@ namespace LeetCode.Tests
 				{
 					Debug.Assert(char.IsNumber(input[i]));
 					int count = input[i] - '0';
-					int l = segmentLength;
-					if (repeatings.Count > 0)
-						l += repeatings.Last().Length;
-					var r = new Repeating(l, count, i - 1);
-					repeatings.Add(r);
+					Repeating r;
+					if (segmentLength > 0)
+					{
+						int l = segmentLength;
+						if (repeatings.Count > 0)
+							l += repeatings.Last().Length;
+						r = new Repeating(l, count, i - 1);
+						repeatings.Add(r);
+						segmentLength = 0;
+					}
+					else
+					{
+						Debug.Assert(repeatings.Count > 0);
+						r = repeatings[repeatings.Count - 1];
+						r.Count *= count;
+						repeatings[repeatings.Count - 1] = r;
+					}
 
-					if (r.Length > K)
+					if (r.Length >= K)
 						break;
-
-					segmentLength = 0;
 				}
 			}
+			if (repeatings.Count == 0)
+				repeatings.Add(new Repeating(input.Length, 1, input.Length - 1));
 
 			Debug.Assert(repeatings.All(r => char.IsLetter(input[r.EndIndex])));
-
+			Debug.Assert(repeatings.Count > 0);
 
 			return repeatings;
 		}
@@ -84,42 +109,11 @@ namespace LeetCode.Tests
 			}
 
 			public int BaseLength { get; }
-			public int Count { get; }
+			public int Count { get; set; }
 
 			public int Length => BaseLength * Count;
 
 			public int EndIndex { get; }
 		}
-
-		public static int DecodeAtIndex(string S)
-		{
-			int totalLength = 0;
-			int segmentLength = 0;
-			for (int i = 0; i < S.Length; i++)
-			{
-				//if (K == 0)
-				//	return S[i].ToString();
-
-
-				if (char.IsLetter(S[i]))
-				{
-					segmentLength++;
-					//K--;
-				}
-				else
-				{
-					Debug.Assert(char.IsNumber(S[i]));
-					int count = S[i] - '0';
-					totalLength = (totalLength + segmentLength) * count;
-
-
-					segmentLength = 0;
-				}
-			}
-
-			return totalLength;
-		}
-
-
 	}
 }
