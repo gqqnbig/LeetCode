642872069c7da380ba217906d93c1137f231adae
gqqnbig <gqqnb2005@gmail.com>
2019-09-27 16:52:32 -0700

    用值缩小范围，用时减少了

--- Tests/493-Reverse Pairs-Tests.cs
+++ Tests/493-Reverse Pairs-Tests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,12 +12,42 @@ namespace LeetCode.Tests
 	public class ImportantReversePairsTests
 	{
 		[DataTestMethod]
-		[DataRow(0,new[]{2147483647, 2147483647, 2147483647, 2147483647, 2147483647, 2147483647})]
-		[DataRow(2,new[]{1,3,2,3,1})]
-		[DataRow(3,new[]{2,4,3,5,1})]
+		[DataRow(19, new[] { 68, 45, 28, 49, 13, 60, 72, 61, 20, 9 })]
+		[DataRow(0, new[] { 2147483647, 2147483647, 2147483647, 2147483647, 2147483647, 2147483647 })]
+		[DataRow(2, new[] { 1, 3, 2, 3, 1 })]
+		[DataRow(3, new[] { 2, 4, 3, 5, 1 })]
 		public void Test(int expected, int[] nums)
 		{
-			Assert.AreEqual(expected,new ImportantReversePairs().ReversePairs(nums));
+			Assert.AreEqual(expected, new ImportantReversePairs().ReversePairs(nums));
+		}
+
+		[TestMethod]
+		public void SpeedTest()
+		{
+			var nums = (from m in Resource1.P493Input1.Split(',')
+						select Convert.ToInt32(m)).ToArray();
+
+			var sw = Stopwatch.StartNew();
+			new ImportantReversePairs().ReversePairs(nums);
+			sw.Stop();
+			Console.WriteLine(sw.ElapsedMilliseconds);
+		}
+
+		[TestMethod]
+		public void GenerationTest()
+		{
+			Random rand = new Random();
+			var solution = new ImportantReversePairs();
+			for (int i = 0; i < 100; i++)
+			{
+				int length = 10;
+				int[] arr = new int[length];
+				for (int j = 0; j < length; j++)
+					arr[j] = rand.Next(100);
+
+
+				Assert.AreEqual(solution.ReversePairsBruteForce(arr), solution.ReversePairs(arr), "nums=" + ExpressionToCodeLib.ObjectToCode.ComplexObjectToPseudoCode(arr));
+			}
 		}
 	}
 }
--- Tests/493-Reverse Pairs.cs
+++ Tests/493-Reverse Pairs.cs
@@ -1,26 +1,106 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Win32.SafeHandles;
+using Newtonsoft.Json;
 
 namespace LeetCode.Tests
 {
 	class ImportantReversePairs
 	{
-		public int ReversePairs(int[] nums)
+		public int ReversePairsBruteForce(int[] nums)
 		{
 			int count = 0;
 			for (int i = 0; i < nums.Length; i++)
 			{
+				var n = nums[i] / 2.0f;
 				for (int j = i + 1; j < nums.Length; j++)
 				{
-					if (nums[i] / 2.0f > nums[j])
+					if (n > nums[j])
 						count++;
 				}
 			}
 
 			return count;
+		}
+
+		public int ReversePairs(int[] nums)
+		{
+			Item[] arr = new Item[nums.Length];
+			for (int i = 0; i < nums.Length; i++)
+				arr[i] = new Item { Index = i, Value = nums[i] };
+
+			Array.Sort(arr, Comparer<Item>.Create((a, b) => b.Value.CompareTo(a.Value)));
+			Debug.Assert(arr[0].Value >= arr[arr.Length - 1].Value, "降序排列");
+
+			var subList = CreateSubList(arr);
+
+			int count = 0;
+			for (int i = 0; i < arr.Length; i++)
+			{
+				var n = arr[i].Value / 2f;
+				var index = arr[i].Index;
+
+				var subListCurrent = subList.First;
+				while (subListCurrent != null && subListCurrent.Value.Value >= n)
+				{
+					var next = subListCurrent.Next;
+					subList.Remove(subListCurrent);
+					subListCurrent = next;
+				}
+
+
+				subListCurrent = subList.First;
+				//排除索引不合要求的项目
+				while (subListCurrent != null)
+				{
+					var next = subListCurrent.Next;
+					if (subListCurrent.Value.Index >= index)
+						count++;
+					subListCurrent = next;
+				}
+			}
+
+			return count;
+		}
+
+		private static LinkedList<Item> CreateSubList(Item[] arr)
+		{
+			var subListStart = 1;
+
+			var n = arr[0].Value / 2f;
+			for (; subListStart < arr.Length && arr[subListStart].Value >= n; subListStart++) { }
+
+			LinkedList<Item> subList = new LinkedList<Item>(arr.Skip(subListStart));
+			return subList;
+		}
+
+		/// <summary>
+		/// 从链接表的指定节点开始复制
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="node"></param>
+		/// <returns></returns>
+		LinkedList<T> Clone<T>(LinkedListNode<T> node)
+		{
+			var list = new LinkedList<T>();
+			while (node != null)
+			{
+				list.AddLast(node.Value);
+				node = node.Next;
+			}
+
+			return list;
+		}
+
+		[DebuggerDisplay("[{Index}]={Value}")]
+		struct Item
+		{
+			public int Value;
+			public int Index;
 		}
 	}
 }
