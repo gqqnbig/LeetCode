fdabd88fa81346e544dba2cdd25167b9d8a7d066
gqqnbig <gqqnb2005@gmail.com>
2019-09-27 15:38:20 -0700

    493. Reverse Pairs 完成基础算法

---
+++ Tests/493-Reverse Pairs-Tests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Tests
+{
+	[TestClass]
+	public class ImportantReversePairsTests
+	{
+		[DataTestMethod]
+		[DataRow(0,new[]{2147483647, 2147483647, 2147483647, 2147483647, 2147483647, 2147483647})]
+		[DataRow(2,new[]{1,3,2,3,1})]
+		[DataRow(3,new[]{2,4,3,5,1})]
+		public void Test(int expected, int[] nums)
+		{
+			Assert.AreEqual(expected,new ImportantReversePairs().ReversePairs(nums));
+		}
+	}
+}
---
+++ Tests/493-Reverse Pairs.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Tests
+{
+	class ImportantReversePairs
+	{
+		public int ReversePairs(int[] nums)
+		{
+			int count = 0;
+			for (int i = 0; i < nums.Length; i++)
+			{
+				for (int j = i + 1; j < nums.Length; j++)
+				{
+					if (nums[i] / 2.0f > nums[j])
+						count++;
+				}
+			}
+
+			return count;
+		}
+	}
+}
