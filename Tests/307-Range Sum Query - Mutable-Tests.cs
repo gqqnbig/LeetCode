0e503a8362069aab4a4f240eb646203a6d9c2a4b
gqqnbig <gqqnb2005@gmail.com>
2019-09-28 18:17:24 -0700

    307. Range Sum Query - Mutable 通过

---
+++ Tests/307-Range Sum Query - Mutable-Tests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Tests
+{
+	[TestClass]
+	public class RangeSumQueryMutableTests
+	{
+		[TestMethod]
+		public void Test1()
+		{
+			var arr = new NumArray(new[] { 1, 3, 5 });
+			Assert.AreEqual(9, arr.SumRange(0, 2));
+
+			arr.Update(1, 2);
+
+			Assert.AreEqual(8, arr.SumRange(0, 2));
+		}
+
+		[TestMethod]
+		public void Test2()
+		{
+			var arr = new NumArray(new[] { 9, -8 });
+			arr.Update(0, 3);
+
+			Assert.AreEqual(-8, arr.SumRange(1, 1));
+
+			Assert.AreEqual(-5, arr.SumRange(0, 1));
+
+			arr.Update(1, -3);
+			Assert.AreEqual(0, arr.SumRange(0, 1));
+		}
+
+	}
+}
---
+++ Tests/307-Range Sum Query - Mutable.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Tests
+{
+	public class NumArray
+	{
+		private BinaryIndexedArray arr;
+
+
+		public NumArray(int[] nums)
+		{
+			arr = new BinaryIndexedArray(nums);
+		}
+
+		public void Update(int i, int val)
+		{
+			arr[i] = val;
+		}
+
+		/// <summary>
+		/// 获取[i,j]区间的和
+		/// </summary>
+		/// <param name="i"></param>
+		/// <param name="j"></param>
+		/// <returns></returns>
+		public int SumRange(int i, int j)
+		{
+			return arr.GetSum(i, j - i + 1);
+		}
+	}
+
+	/**
+	 * Your NumArray object will be instantiated and called as such:
+	 * NumArray obj = new NumArray(nums);
+	 * obj.Update(i,val);
+	 * int param_2 = obj.SumRange(i,j);
+	 */
+}
