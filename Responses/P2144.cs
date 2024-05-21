5d78519326ec8dc987330acb043c85dedcfd3808
QQ喵 <gqqnb2005@gmail.com>
2024-05-21 20:21:03 +0800

    2144. Minimum Cost of Buying Candies With Discount

---
+++ Responses/P2144.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+	class P2144
+	{
+		public int MinimumCost(int[] cost)
+		{
+			Array.Sort(cost);
+
+
+			int bought = 0;
+			int spend = 0;
+			for (int i = cost.Length - 1; i >= 0; i--)
+			{
+				if (bought == 2)
+				{
+					//take this candy for free.
+					bought = 0;
+				}
+				else
+				{
+					spend += cost[i];
+					bought++;
+				}
+			}
+
+			return spend;
+		}
+
+		public static void Main(string[] args)
+		{
+			var p = new P2144();
+			Console.WriteLine(p.MinimumCost(new int[] { 1, 2, 3 }));
+			Console.WriteLine(p.MinimumCost(new int[] { 6, 5, 7, 9, 2, 2 }));
+
+		}
+	}
+}
