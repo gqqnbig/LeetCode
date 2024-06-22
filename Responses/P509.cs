33bd5a78d9093d000f9afed923da527bdf7b55eb
QQ喵 <gqqnb2005@gmail.com>
2024-06-22 17:37:15 +0800

    using the characteristic equation of the linear homogeneous relation

---
+++ Responses/P509.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Responses
+{
+	class P509
+	{
+		public int Fib(int n)
+		{
+			double sq5 = Math.Sqrt(5);
+
+			double term1 = 1 / sq5 * Math.Pow((1 + sq5) / 2, n);
+			double term2 = -1 / sq5 * Math.Pow((1 - sq5) / 2, n);
+
+			return (int)Math.Round(term1 + term2);
+		}
+
+
+		public static void Main(string[] args)
+		{
+			var p = new P509();
+			Console.WriteLine(p.Fib(1));
+			Console.WriteLine(p.Fib(2));
+			Console.WriteLine(p.Fib(3));
+			Console.WriteLine(p.Fib(4));
+			Console.WriteLine(p.Fib(5));
+
+
+		}
+	}
+}
